dea7c66c78e73e8101514a14e8f4d0efc2700d80
Oussama Tchah <oussamatchah1@gmail.com>
2021-07-01 14:46:12 +0100

    first commit

---
+++ src/main/java/test/sast/SastApplication.java
@@ -0,0 +1,13 @@
+package test.sast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SastApplication.class, args);
+	}
+
+}
